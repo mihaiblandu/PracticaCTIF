9ebc85668fa09d7643af8368932c1df200a40d53
Bogdan <gritcub@gmail.com>
2025-05-21 22:05:23 +0300

    https support for backend

--- project/app/register/index.tsx
+++ project/app/register/index.tsx
@@ -26,7 +26,7 @@ export default function RegisterScreen() {
 
       setIsLoading(true);
 
-      const response = await fetch('http://localhost:8080/api/auth/register', {
+      const response = await fetch('https://localhost:8443/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
